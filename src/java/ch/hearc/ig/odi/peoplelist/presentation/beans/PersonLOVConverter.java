eff5f56e62d0a8aaecbc87606954adfde60e30e5
dardankastrati <dardan.kastrati@IG15-KASTRATID.intra.eiaj.ch>
2016-10-27 08:59:09 +0200

    Ajout du convertisseur natif pour la date

---
+++ src/java/ch/hearc/ig/odi/peoplelist/presentation/beans/PersonLOVConverter.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.odi.peoplelist.presentation.beans;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
+/**
+ *
+ * @author dardan.kastrati
+ */
+@Named(value = "personLOVConverter")
+@Dependent
+public class PersonLOVConverter implements Converter{
+
+    /**
+     * Creates a new instance of PersonLOVConverter
+     */
+    public PersonLOVConverter() {
+    }
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
